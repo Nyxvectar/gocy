5806e47c03773c6b476d4ea018486ec63b92f6f3
Nyxvectar <Nyxvectar@proton.me>
2025-07-07 14:24:05 +0800

    Files Sorted

--- main.go
+++ main.go
@@ -3,26 +3,36 @@ package main
 import (
 	"fmt"
 	"gocy/class"
+	"gocy/commandsl"
 )
 
 func main() {
-	//initation()
-	//commandsl.CalculateG()
-	//commandsl.ForWhile()
-	//commandsl.LogicalT()
-	//commandsl.YearCheck(2002)
-	//commandsl.SwitchTest2()
-	//commandsl.ForL(10)
-	//commandsl.Infinity(10)
-	//commandsl.Variables()
-	//commandsl.Randomly(50)
-	class.Piggy()
+	initation(false)
+	switchCmd(false)
+	//class.Piggy()
+	class.Consts()
 }
 
-func initation() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+func switchCmd(whether bool) {
+	if whether == true {
+		commandsl.CalculateG()
+		commandsl.ForWhile()
+		commandsl.LogicalT()
+		commandsl.YearCheck(2002)
+		commandsl.SwitchTest2()
+		commandsl.ForL(10)
+		commandsl.Infinity(10)
+		commandsl.Variables()
+		commandsl.Randomly(50)
+	}
+}
+
+func initation(whether bool) {
+	if whether == true {
+		s := "gopher"
+		fmt.Printf("Hello and welcome, %s!\n", s)
+		for i := 1; i <= 5; i++ {
+			fmt.Println("i =", 100/i)
+		}
 	}
 }
