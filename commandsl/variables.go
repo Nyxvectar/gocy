dfec5617f856ef800054f4e391c0fd643f89ac3d
Nyxvectar <Nyxvectar@proton.me>
2025-07-07 14:01:23 +0800

    Added var and :=

--- commandsl/variables.go
+++ commandsl/variables.go
@@ -1,5 +1,7 @@
 package commandsl
 
+import "math/rand"
+
 var x = 10
 
 func Variables() {
@@ -8,8 +10,27 @@ func Variables() {
 		println(y)
 	}
 	var y = 30
+	// These sentense has a equally effect of this:
+	// y := 30
+	// This form can be used in some area where we
+	// can not use the normal var keyword.
+	// The examble is put below, around 5 lines. A and B.
 	println(x, y)
 	// The expexted y output is 30 but not 20
 	// because the {} declares a domain of each
 	// variable.
+	if true {
+		// TODO: Example A
+		for counter := 10; counter > 0; counter-- {
+			println(counter)
+		}
+		// TODO: Example B
+		if numb := rand.Intn(3); numb == 0 {
+			println("Space Adventures")
+		} else if numb == 1 {
+			println("China Voyager")
+		} else {
+			println("Virgin Galactic")
+		}
+	}
 }
